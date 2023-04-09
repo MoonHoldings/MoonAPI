193a4c2ec774db95ec7921dfcec2249aa20b392f
Vince G <allen_gonzales@dlsu.edu.ph>
2023-04-09 10:42:20 +0800

    Add pre commit

--- src/resolvers/Loan.ts
+++ src/resolvers/Loan.ts
@@ -4,7 +4,7 @@ import { Resolver, Query, Arg } from "type-graphql"
 @Resolver()
 export class LoanResolver {
   @Query(() => [Loan])
-  async getLoan(@Arg("id", () => [Number]) id: number): Promise<Loan> {
+  async getLoan(@Arg("id", () => Number) id: number): Promise<Loan> {
     return await Loan.findOneOrFail({
       where: { id },
     })
