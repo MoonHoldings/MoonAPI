aa547b0e3e351cd55f39bd27466a44e676327b85
Allen Vincent Gonzales <vincegonzalesdlsu@gmail.com>
2023-04-17 19:39:50 +0800

    Minor fixes

--- src/resolvers/User.ts
+++ src/resolvers/User.ts
@@ -7,34 +7,38 @@ import * as utils from '../utils'
 
 @Resolver()
 export class UserResolver {
+  @Mutation(() => User)
+  async register(@Arg('email') email: string, @Arg('password') password: string): Promise<User> {
+    return await userService.register(email, password)
+  }
 
-    @Mutation(() => User)
-    async register(@Arg('email') email: string, @Arg('password') password: string): Promise<User> {
-        return await userService.register(email, password)
-    }
+  @Mutation(() => User)
+  async login(@Arg('email') email: string, @Arg('password') password: string, @Ctx() ctx: Context<any>): Promise<User> {
+    return await userService.login(email, password, ctx)
+  }
 
-    @Mutation(() => User)
-    async login(@Arg('email') email: string, @Arg('password') password: string, @Ctx() ctx: Context<any>): Promise<User> {
-        return await userService.login(email, password, ctx)
-    }
+  //like a logout function
+  @Mutation(() => Boolean)
+  @UseMiddleware(isAuth)
+  async revokeRefreshTokensForUser(@Arg('userId', () => Int) userId: number) {
+    await userService.incrementRefreshVersion(userId)
+    return true
+  }
 
-    //like a logout function
-    @Mutation(() => Boolean)
-    @UseMiddleware(isAuth)
-    async revokeRefreshTokensForUser(@Arg('userId', () => Int) userId: number) {
-        await userService.incrementRefreshVersion(userId)
-        return true
-    }
+  @Query(() => String)
+  async generateDiscordUrl() {
+    return utils.generateDiscordUrl()
+  }
 
-    @Query(() => String)
-    async generateDiscordUrl() {
-        return utils.generateDiscordUrl()
-    }
+  @Query(() => Boolean)
+  async getPasswordResetUrl(@Arg('email') email: string): Promise<boolean> {
+    return await userService.getPasswordResetEmail(email)
+  }
 
-    @Query(() => Boolean)
-    async getPasswordResetUrl(@Arg('email') email: string,): Promise<boolean> {
-        return await userService.getPasswordResetEmail(email);
-    }
+  @Query(() => Boolean)
+  @UseMiddleware(isAuth)
+  async updatePassword(@Arg('password') email: string, @Ctx() ctx: Context<any>): Promise<boolean> {
+    const token = ctx.req.cookies.jid
 
     @Query(() => Boolean)
     async updatePassword(@Arg('password') email: string, @Ctx() ctx: Context<any>): Promise<boolean> {
@@ -42,25 +46,15 @@ export class UserResolver {
         return await userService.updatePassword(email, token);
     }
 
-
-    // @Query(() => String)
-    // async bye(@Ctx() { }: Session) {
-    //     const DiscordOauth2 = require("discord-oauth2");
-    //     const oauth = new DiscordOauth2({
-    //         clientId: '1096313631894933544',
-    //         clientSecret: 'fnOfTnPPDypUl7fyDt6wVjFdPyuZLEwP',
-    //         redirectUri: 'http://localhost:80/auth/discord',
-    //     });
-
-    //     const url = oauth.generateAuthUrl({
-    //         scope: ["identify", "email"],
-    //         state: crypto.randomBytes(16).toString("hex"),
-    //     });
-    //     return url;
-    // }
-    //TODO: In case to use in future
-    // @Query(() => User)
-    // async refreshToken(@Ctx() ctx: Session): Promise<String> {
-    //     return await this.userService.refreshAccessToken(ctx);
-    // }
+  //     const url = oauth.generateAuthUrl({
+  //         scope: ["identify", "email"],
+  //         state: crypto.randomBytes(16).toString("hex"),
+  //     });
+  //     return url;
+  // }
+  //TODO: In case to use in future
+  // @Query(() => User)
+  // async refreshToken(@Ctx() ctx: Session): Promise<String> {
+  //     return await this.userService.refreshAccessToken(ctx);
+  // }
 }
