d7d094f42af1166c436aaf9d8d51252be8bd303d
Allen Vincent Gonzales <vincegonzalesdlsu@gmail.com>
2023-04-18 09:26:28 +0800

    Fix build

--- dist/enums/SignupType.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SigninType = void 0;
-var SigninType;
-(function (SigninType) {
-    SigninType["EMAIL"] = "EMAIL";
-    SigninType["DISCORD"] = "DISCORD";
-})(SigninType = exports.SigninType || (exports.SigninType = {}));
-//# sourceMappingURL=SignupType.js.map
