62830622f9a8e26427dde7fdffb31a26844b59c7
Allen Vincent Gonzales <vincegonzalesdlsu@gmail.com>
2023-04-18 12:00:23 +0800

    Fix dist and vercel config

--- dist/enums/EmailTokenType.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.EmailTokenType = void 0;
-var EmailTokenType;
-(function (EmailTokenType) {
-    EmailTokenType["CONFIRMATION_EMAIL"] = "CONFIRMATION_EMAIL";
-    EmailTokenType["RESET_PASSWORD"] = "RESET_PASSWORD";
-})(EmailTokenType = exports.EmailTokenType || (exports.EmailTokenType = {}));
-//# sourceMappingURL=EmailTokenType.js.map
