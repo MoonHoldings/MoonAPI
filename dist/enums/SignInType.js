1415a2c5d5443a768b7e451cd32db453e127223e
Vince Gonzales <vincegonzalesdlsu@gmail.com>
2023-04-18 11:50:09 +0800

    Fix dist

---
+++ dist/enums/SignInType.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.SignInType = void 0;
+var SignInType;
+(function (SignInType) {
+    SignInType["EMAIL"] = "EMAIL";
+    SignInType["DISCORD"] = "DISCORD";
+})(SignInType = exports.SignInType || (exports.SignInType = {}));
+//# sourceMappingURL=SignInType.js.map
