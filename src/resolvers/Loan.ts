944c7427021cf152a44f70561b53896162e10922
Vince Gonzales <vincegonzalesdlsu@gmail.com>
2023-04-30 11:28:36 +0800

    Authenticate sharkify resolvers

--- src/resolvers/Loan.ts
+++ src/resolvers/Loan.ts
@@ -1,20 +1,23 @@
-import { Resolver, Query, Arg } from 'type-graphql'
+import { Resolver, Query, Arg, Authorized } from 'type-graphql'
 import * as loanService from '../services/Loan'
 import { Loan } from '../entities'
-import { GetLoansArgs, HistoricalLoanResponse, PaginatedLoanResponse } from '../types'
+import { GetLoansArgs, HistoricalLoanResponse, PaginatedLoanResponse, UserRole } from '../types'
 
 @Resolver()
 export class LoanResolver {
+  @Authorized(UserRole.Superuser)
   @Query(() => Loan)
   async getLoan(@Arg('id', () => Number) id: number): Promise<Loan> {
     return await loanService.getLoanById(id)
   }
 
+  @Authorized(UserRole.Superuser)
   @Query(() => PaginatedLoanResponse)
   async getLoans(@Arg('args', { nullable: true }) args: GetLoansArgs): Promise<PaginatedLoanResponse> {
     return await loanService.getLoans(args)
   }
 
+  @Authorized(UserRole.Superuser)
   @Query(() => [HistoricalLoanResponse], { nullable: true })
   async getHistoricalLoansByUser(
     @Arg('borrower', { nullable: true }) borrower: string,
