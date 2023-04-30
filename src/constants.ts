24fb93082b80bb1aab33799e7bd7ba5e11f9adb4
Vince Gonzales <vincegonzalesdlsu@gmail.com>
2023-04-30 12:50:05 +0800

    Add token decryption

--- src/constants.ts
+++ src/constants.ts
@@ -19,6 +19,7 @@ export const DISCORD_CLIENT_SECRET = process.env.DISCORD_CLIENT_SECRET
 export const SERVER_URL = process.env.SERVER_URL
 export const WEBAPP_URL = process.env.WEBAPP_URL
 export const COOKIE_DOMAIN = process.env.COOKIE_DOMAIN
+export const CRYPTO_SECRET = process.env.CRYPTO_SECRET
 
 export const AXIOS_CONFIG_SHYFT_KEY = {
   headers: {
