c95571edf467bfe4fbbef029c9f83426bc2bee68
micohalvarez <micohalvarez@yahoo.com>
2023-05-23 13:14:25 +0800

    added sol balance

--- src/utils/shyft.ts
+++ src/utils/shyft.ts
@@ -1,4 +1,4 @@
-import { SHYFT_KEY } from "../constants";
-import { ShyftSdk, Network } from '@shyft-to/js';
+import { SHYFT_KEY } from '../constants'
+import { ShyftSdk, Network } from '@shyft-to/js'
 
-export const shyft = new ShyftSdk({ apiKey: SHYFT_KEY, network: Network.Mainnet });
+export const shyft = new ShyftSdk({ apiKey: SHYFT_KEY, network: Network.Mainnet })
