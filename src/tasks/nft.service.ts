4f6f519da3f25add81bcf23821608c54ea991f5f
Vince Gonzales <vincegonzalesdlsu@gmail.com>
2023-05-26 12:14:36 +0800

    Remove unused import

--- src/tasks/nft.service.ts
+++ src/tasks/nft.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common'
-import { Interval, Timeout } from '@nestjs/schedule'
+import { Interval } from '@nestjs/schedule'
 import * as nftService from '../services/Nft'
 
 @Injectable()
