77a7b91cf7f5985b72263261bf25cd1850bcf02a
Vince Gonzales <vincegonzalesdlsu@gmail.com>
2023-06-29 13:42:23 +0800

    Update sharky client

--- src/utils/sharkyClient.ts
+++ src/utils/sharkyClient.ts
@@ -2,6 +2,6 @@ import { createSharkyClient } from '@sharkyfi/client'
 import createAnchorProvider from './createAnchorProvider'
 
 const provider = createAnchorProvider()
-const sharkyClient = createSharkyClient(provider)
+const sharkyClient = createSharkyClient(provider, undefined, 'mainnet')
 
 export default sharkyClient
